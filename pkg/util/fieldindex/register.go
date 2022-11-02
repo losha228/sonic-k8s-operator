4c1b99bb5ee38e263b748004565dad6a496f237a
Longquan Sha <losha@microsoft.com>
2022-11-02 15:28:59 +0800

    Update

--- pkg/util/fieldindex/register.go
+++ pkg/util/fieldindex/register.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 The Kruise Authors.
+Copyright 2019 The Sonic_k8s Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
